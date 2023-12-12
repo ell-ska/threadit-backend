ec67d27b97ff23b412899d2a9b564bff6a40b696
ell-ska <ella@skagerstrom.se>
2023-12-12 14:24:31 +0100

    add cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import cors from 'cors'
 
 import 'dotenv/config'
 import * as authController from './controllers/auth'
 import validateToken from './middleware/validateToken'
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
-app.post('/register', authController.register)
-app.post('/login', authController.login)
+app.post('/sign-up', authController.register)
+app.post('/sign-in', authController.login)
 app.get('/profile', validateToken, authController.profile)
 
 const mongoUrl = process.env.DB_URL
